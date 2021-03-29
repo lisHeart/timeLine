2447cf08eb8beb87364549f80c52c661a09c15f6
liu gaoyang <feiyangto@163.com>
2021-03-29 14:12:52 +0800

    修改bug

--- YWTimeLine/dataMap.js
+++ YWTimeLine/dataMap.js
@@ -2,7 +2,7 @@ import TimelineItem from './timelineItem'
 
 export function getPicName(id) {
   switch (id) {
-    case 1:
+    case 11111111:
       return 'https://user-images.githubusercontent.com/12069729/36057805-80cfc3d2-0e4e-11e8-8851-6fda091ff389.png'
     case 2:
       return 'https://user-images.githubusercontent.com/12069729/36057805-80cfc3d2-0e4e-11e8-8851-6fda091ff389.png'
